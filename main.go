20cfbf1e136a7b84a17580ca6958297a61b3c54d
IrakliGiorgadze <ika.giorgadze@gmail.com>
2023-09-18 23:06:30 +0400

    add different pages for testing

--- main.go
+++ main.go
@@ -5,18 +5,43 @@ import (
 	"net/http"
 )
 
+const (
+	webPort = "3000"
+)
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting the server on :3000...")
-	_ = http.ListenAndServe(":3000", nil)
+	var router Router
+	fmt.Println("Starting the server on port:", webPort)
+	_ = http.ListenAndServe(fmt.Sprintf(":%s", webPort), router)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site</h1>")
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site</h1>")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at "+
+		"<a href=\"mailto:ika.giorgadze@gmail.com\">ika.giorgadze@gmail.com</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>`)
 }
