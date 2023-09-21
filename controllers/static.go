60b90eeeffb7d09cdac807046cc119325fddcd17
IrakliGiorgadze <ika.giorgadze@gmail.com>
2023-09-21 16:23:00 +0400

    add signup page

--- controllers/static.go
+++ controllers/static.go
@@ -2,32 +2,34 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/IrakliGiorgadze/go-web-app/views"
 )
 
-func StaticHandler(tpl views.Template) http.HandlerFunc {
+func StaticHandler(tpl Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	}
 }
 
-func FAQ(tpl views.Template) http.HandlerFunc {
+func FAQ(tpl Template) http.HandlerFunc {
 	questions := []struct {
 		Question string
 		Answer   string
 	}{
 		{
-			Question: "Is there a free version?",
-			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+			Question: "What are two things you can never eat for breakfast?",
+			Answer:   "Lunch and Dinner.",
 		},
 		{
-			Question: "What are your support hours?",
-			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+			Question: "What is always coming but never arrives?",
+			Answer:   "Tomorrow",
 		},
 		{
-			Question: "How do I contact support?",
-			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+			Question: "What gets wetter the more it dries?",
+			Answer:   `A towel`,
+		},
+		{
+			Question: "What can be broken but never held?",
+			Answer:   `A promise`,
 		},
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
