144ca62a8d847b4324dd9df3e4bd5f0042adebc0
IrakliGiorgadze <ika.giorgadze@gmail.com>
2023-10-13 16:36:40 +0400

    serving static assets

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../templates/**/*.{gohtml,html}"],
+  content: ["/templates/**/*.{gohtml,html}"], // add ../templates/ in case you want to run locally
   theme: {
     extend: {},
   },
